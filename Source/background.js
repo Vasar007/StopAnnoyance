8260c4e7ff98b9e2c73299694e712b39ed622a48
Vasily Vasilyev <vasar007@yandex.ru>
2019-08-04 15:30:33 +0300

    Change icons

--- Source/background.js
+++ Source/background.js
@@ -1,6 +1,10 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener((tab) => {
+    chrome.storage.sync.set({color: '#e8453c'}, function() {
+        console.log('The color is red.');
+    });
+
     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
